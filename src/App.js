de4cc1cc804b91e514cfd31d91c2b0372b0808d8
Chi-Heng Huang <polozero1@yahoo.com.tw>
2019-07-10 22:11:24 +0800

    update dependency & add reset css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/normalize.css';
+import './assets/styles/base.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
