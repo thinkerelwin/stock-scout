79c5fb6d8114bcfa915f5bde8cad9fc0dc96b873
Huang Chi-Heng <huangchi-heng@Huangs-MacBook-Pro.local>
2020-02-10 23:49:35 +0800

    add example sort function and custom data display in a cell

--- src/features/screener/Screener.js
+++ src/features/screener/Screener.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useRouteMatch, Switch, Route } from 'react-router-dom';
 import { Column, Table } from 'react-virtualized';
-import 'react-virtualized/styles.css';
 
 import Categories from './Categories';
+import sectorList from '../../utils/sector-stocks.json';
 
+import 'react-virtualized/styles.css';
 import './Screener.scss';
 
 const topList = [
@@ -16,7 +17,32 @@ const topList = [
 const Screener = () => {
   let { path } = useRouteMatch();
   const [currentTab, setCurrentTab] = useState(topList[0].url);
+  console.log(sectorList);
 
+  const [screenerList, setScreenerList] = useState(sectorList);
+  const [sortBy, setSortBy] = useState('symbol');
+  const [sortDirection, setSortDirection] = useState('ASC');
+
+  function _sort({ sortBy, sortDirection }) {
+    const sortedList = _sortList({ sortBy, sortDirection });
+
+    setScreenerList(sortedList);
+    setSortBy(sortBy);
+    setSortDirection(sortDirection);
+  }
+
+  function _sortList({ sortBy, sortDirection }) {
+    return screenerList.reverse();
+  }
+
+  function doubleDataRowRender({ cellData, columnData, rowIndex }) {
+    // console.log(cellData, columnData, rowIndex);
+    if (cellData == null) {
+      return '';
+    } else {
+      return String(`${cellData} \n ${columnData.name[rowIndex]}`);
+    }
+  }
   return (
     <main className="screener menu-margin">
       <div className="screener-box">
@@ -30,15 +56,52 @@ const Screener = () => {
           <Route path={`${path}/:category`}>
             {/* TODO example needs to be modified */}
             <Table
-              width={300}
+              width={1000}
               height={300}
               headerHeight={20}
               rowHeight={30}
-              rowCount={topList.length}
-              rowGetter={({ index }) => topList[index]}
+              rowCount={sectorList.length}
+              rowGetter={({ index }) => sectorList[index]}
+              sort={_sort}
+              sortBy={sortBy}
+              sortDirection={sortDirection}
             >
-              <Column width={200} label="Description" dataKey="url" />
-              <Column label="Name" dataKey="name" width={100} />
+              <Column
+                // cellDataGetter={dobleRowDataGetter}
+                columnData={{ name: sectorList.map(item => item.companyName) }}
+                cellRenderer={doubleDataRowRender}
+                label="Description"
+                dataKey="symbol"
+                width={200}
+                flexGrow={1}
+              />
+              <Column
+                label="Name"
+                dataKey="companyName"
+                width={100}
+                flexGrow={1}
+              />
+              <Column
+                label="Last"
+                dataKey="latestPrice"
+                width={100}
+                flexGrow={1}
+              />
+              <Column
+                label="CHG %"
+                dataKey="changePercent"
+                width={100}
+                flexGrow={1}
+              />
+              <Column label="CHG" dataKey="change" width={100} flexGrow={1} />
+              <Column label="VOL" dataKey="volume" width={100} flexGrow={1} />
+              <Column
+                label="MKT CAP"
+                dataKey="marketCap"
+                width={100}
+                flexGrow={1}
+              />
+              <Column label="P/E" dataKey="peRatio" width={100} flexGrow={1} />
             </Table>
             ,
             <Route path={`${path}/:category/:tab`}>
