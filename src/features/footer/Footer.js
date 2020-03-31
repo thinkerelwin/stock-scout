b8c7022e2cad3acf379679b4f99c7bb401188d0f
thinkerelwin <elwin@mitake.com.tw>
2020-03-31 09:18:42 +0800

    fix capitalization on file & folder names

---
+++ src/features/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <small className="copyright">© Copyright 2020 by Elwin Huang.</small>
+      <a className="provider" href="https://iexcloud.io">
+        Data provided by IEX Cloud
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/features/screener/screenerTableSlice.js
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import instance from '../../api/IEXCloud';
+
+let initialState = {
+  screenerList: [],
+  isFetchingList: false,
+  listError: null
+};
+
+const screenerTableSlice = createSlice({
+  name: 'screenerTable',
+  initialState,
+  reducers: {
+    setIsFetchingList(state, action) {
+      state.isFetchingList = action.payload;
+    },
+    setScreenerTableSuccess(state, action) {
+      state.screenerList = action.payload.screenerList;
+      state.listError = null;
+    },
+    setScreenerTableFailed(state, action) {
+      state.listError = action.payload;
+    }
+  }
+});
+
+export const {
+  setIsFetchingList,
+  setScreenerTableSuccess,
+  setScreenerTableFailed
+} = screenerTableSlice.actions;
+
+export default screenerTableSlice.reducer;
+
+export function fetchscreenerTable(CollectionType, currentTab) {
+  return async dispatch => {
+    dispatch(setIsFetchingList(true));
+    try {
+      const { data } = await instance.get(
+        `/stock/market/collection/${CollectionType}`,
+        {
+          params: {
+            collectionName: currentTab
+          }
+        }
+      );
+
+      dispatch(setScreenerTableSuccess({ screenerList: data }));
+      dispatch(setIsFetchingList(false));
+    } catch (err) {
+      dispatch(setScreenerTableFailed(err.toString()));
+      dispatch(setIsFetchingList(false));
+    }
+  };
+}
